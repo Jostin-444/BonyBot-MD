9206a21f9404f8703edcb9469c20cdad00b99b32
Jostin-444 <gemersport7@gmail.com>
2024-11-07 14:29:04 -0600

    Actualizar downloader-spotify.js

--- plugins/downloader-spotify.js
+++ plugins/downloader-spotify.js
@@ -1,13 +1,13 @@
 import Scraper from "@SumiFX/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return m.reply('🍭 Ingresa el nombre de algún Track de Spotify.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`)
+if (!text) return m.reply('🥕 Ingresa el nombre de algún Track de Spotify.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`)
 
 let user = global.db.data.users[m.sender]
 try {
 let { title, artist, album, published, thumbnail, dl_url } = await Scraper.spotify(text)
 let txt = `╭─⬣「 *Spotify Download* 」⬣\n`
-    txt += `│  ≡◦ *🍭 Nombre ∙* ${title}\n`
+    txt += `│  ≡◦ *🥕 Nombre ∙* ${title}\n`
     txt += `│  ≡◦ *🪴 Artista ∙* ${artist}\n`
     txt += `│  ≡◦ *📚 Album ∙* ${album}\n`
     txt += `│  ≡◦ *📅 Publicado ∙* ${published}\n`
